31129ea67b4ddcf3a893aaddef6cd0c5d671d2ba
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-05 10:28:00 +0100

    added while(True) and getZipCodeOfStudentAddress function

--- Python/university_recod_system.py
+++ Python/university_recod_system.py
@@ -21,48 +21,66 @@ def displayCoursesOfStudent(department, username):
 					return student[details]
 
 
+def getZipCodeOfStudentAddress(department, username):
+	for student in department:
+		if student == username:
+			student_address = department[student]['Address']
+			student_address_list = student_address.split("/")
+			zip_code = student_address_list[-1]
+	return zip_code
+	
+
+
+
 
 
 department = {} 
 #student_details = {}
 student_list = []
-courses = []
+courses = set()
 	
 {'Anna': {'Name': 'Chiamaka', 'Age': '12', 'Address': 'tyu 87 gh', 'Courses': ['physics']}}
-
-student_name = input("Enter Student's name: ")
-while student_name.isalpha() == False:
-	print("Invalid Name. Please ", end= "")
+while True:
 	student_name = input("Enter Student's name: ")
-student_age = int(input("Enter Student's age: "))
-while student_name.isdigit() == False:
-	print("Invalid age. Please ", end= "")
+	while student_name.isalpha() == False:
+		print("Invalid Name. Please ", end= "")
+		student_name = input("Enter Student's name: ")
+	
 	student_age = input("Enter Student's age: ")
-student_courses = input("Enter courses offered by the student: ")
-while student_name.isalpha() == False:
-	print("Invalid course name. Please ", end= "")
+	while student_age.isdigit() == False:
+		print("Invalid age. Please ", end= "")
+		student_age = int(input("Enter Student's age: "))
+
 	student_courses = input("Enter courses offered by the student: ")
+	courses.add(student_courses)
+	user_response = input("Do you want to enter another course(yes/no): ")
+	user_response = user_response.lower()
+	while user_response != "no":
+		student_courses = input("Enter courses offered by the student: ")
+		courses.add(student_courses)
+		user_response = input("Do you want to enter another course(yes/no): ")
 
-courses.append(student_courses)
-student_address = input("Enter Student's address: ")
-
-student_list.append(student_name)
-student_list.append(student_age)
-student_list.append(student_address)
-student_list.append(courses)
-
-student_username = input("Create a unique username: ")
-
-
- 
-student_details = getStudentRecord(student_list)
-print(student_details)
-
-print(getAllStudentsRecords(student_username, student_details, department))
+	while student_courses.isalpha() == False:
+		print("Invalid course name. Please ", end= "")
+		student_courses = input("Enter courses offered by the student: ")
 
 
+	student_address = input("Enter Student's address(house_number/strt_name_/city/state/zip_code): ")
+	
+	student_list.insert(0, student_name)
+	student_list.insert(1, student_age)
+	student_list.insert(2, student_address)
+	student_list.insert(3, courses)
 
+	student_username = input("Create a unique username: ")
+	print(student_list)
+	student_details = getStudentRecord(student_list)
+	print(student_details)
 
+	print(getAllStudentsRecords(student_username, student_details, department))
+
+	
+	print(getZipCodeOfStudentAddress(department, 'Annabeauty'))
 
 
 
