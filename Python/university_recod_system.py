41ff2b279732af2437a3d111e44cb1ba2f1229ae
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-05 08:10:52 +0100

    added displayStudentRecord and dispalyCoursesOfStudent functions

---
+++ Python/university_recod_system.py
@@ -0,0 +1,40 @@
+def displayStudentRecord(department, username):
+	return department[useranme]
+	
+def dispalyCoursesOfStudent(department, username):
+	for student in department:
+		if student == username:
+			for details, data in student.items():
+				if details = 'courses':
+					return student[details]
+
+
+department = {}
+student_details = {}
+student_list = []
+
+
+student_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
