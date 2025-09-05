1bc76819249bdbd5082d222d365bab8d064b1861
Chiamie <okechukwurchiamaka@gmail.com>
2025-09-05 16:06:43 +0100

    added match cases:case 1 out of 4 cases to be added. The case 1 is about Adding student

--- Python/university_recod_system.py
+++ Python/university_recod_system.py
@@ -1,3 +1,28 @@
+def display_main_menu():
+	menu = """
+	1. Student
+	2. Department Subjects
+	"""
+	print(menu)
+
+def display_student_menu():
+	student_menu = """
+			1. Add Student
+			2. View Student Profile
+			3. Update Student Profile
+			4. View Student Subjects
+			"""
+	print(student_menu)
+	
+
+def display_student_profile_menu():
+	student_profile__menu = """
+			1. View All Details
+			2. View City
+			3. View Zip Code
+			"""
+	print(student_profile__menu)
+
 def get_student_record(student_list):
 	student_details = {}
 	student_details['Name'] = student_list[0]
@@ -73,72 +98,72 @@ def displaySaveIcon():
 department_courses = ["Math", "Physics", "Computer Science", "Biology", "Chemistry", "Statistics", "English", "Economics", "History", "Philosophy", "Sociology",
 "Political Science", "Art", "Music", "Engineering", "Law", "Medicine", "Business", "Psychology"]
 department = {} 
-#student_details = {}
 student_list = []
 
-user_input = 'yes'
-while user_input != "-1":
-	courses = set()
-	student_name = input("Enter Student's name: ")
-	while student_name.isalpha() == False:
-		print("Invalid Name. Please ", end= "")
-		student_name = input("Enter Student's name: ")
+department_portal = True
+while department_portal:
+	display_main_menu()
+	main_menu_selection = int(input("Select an option: "))
+	match(main_menu_selection):
+		case 1:
+			display_student_menu()
+			student_menu_selection = int(input("Select an option: "))
+			match(student_menu_selection):
+				case 1:
+					courses = set()
+					student_name = input("Enter Student's name: ")
+					while student_name.isalpha() == False:
+						print("Invalid Name. Please ", end= "")
+						student_name = input("Enter Student's name: ")
 	
-	student_age = input("Enter Student's age: ")
-	while student_age.isdigit() == False:
-		print("Invalid age. Please ", end= "")
-		student_age = input("Enter Student's age: ")
+					student_age = input("Enter Student's age: ")
+					while student_age.isdigit() == False:
+						print("Invalid age. Please ", end= "")
+						student_age = input("Enter Student's age: ")
 
-	student_course = input("Enter course offered by the student: ")
-	student_course = student_course.charAt(0).lower()
-	courses.add(student_course)
-	user_response = input("Do you want to enter another course(yes/no): ")
-	user_response = user_response.lower()
-	while user_response != "no":
-		student_course = input("Enter course offered by the student: ")
-		student_course = student_course.charAt(0).lower()
-		courses.add(student_course)
-		user_response = input("Do you want to enter another course(yes/no): ")
+					student_course = input("Enter course offered by the student: ")
+					while student_course.isalpha() == False:
+						print("Invalid course name. Please ", end= "")
+						student_course = input("Enter course offered by the student: ")
 
-	while student_course.isalpha() == False:
-		print("Invalid course name. Please ", end= "")
-		student_course = input("Enter course offered by the student: ")
-		courses.add(student_course)
+					student_course = student_course.capitalize()
+					courses.add(student_course)
+					user_response = input("Do you want to enter another course(yes/no): ")
+					user_response = user_response.lower()
+					while user_response != "no":
+						student_course = input("Enter course offered by the student: ")
+						while student_course.isalpha() == False:
+							print("Invalid course name. Please ", end= "")
+							student_course = input("Enter course offered by the student: ")
+						student_course = student_course.capitalize()
+						courses.add(student_course)
+						user_response = input("Do you want to enter another course(yes/no): ")
+						user_response = user_response.lower()
 
-	student_address = input("Enter Student's address(house_number/strt_name/city/state/zip_code): ")
+					
+					student_address = input("Enter Student's address(house_number/strt_name/city/state/zip_code): ")
 	
-	student_list.insert(0, student_name)
-	student_list.insert(1, student_age)
-	student_list.insert(2, student_address)
-	student_list.insert(3, courses)
+					student_list.insert(0, student_name)
+					student_list.insert(1, student_age)
+					student_list.insert(2, student_address)
+					student_list.insert(3, courses)
 
-	student_username = input("Create a unique username: ")
-	print(student_list)
-	student_details = get_student_record(student_list)
-	print(student_details)
+					student_username = input("Create a unique username: ")
+					print(student_list)
+			
+					student_details = get_student_record(student_list)
+					print(student_details)
 
-	print(get_all_students_records(student_username, student_details, department))
+					department = get_all_students_records(student_username, student_details, department)
 
-	user_input = input("Do you want to perform another one: ")
-	
-print(get_zip_code_of_student_address(department, 'Annabeauty'))
-	
-print(get_city_of_student_address(department, 'Annabeauty'))
+					
 
 
-student_courses = get_courses_student_offer(department, 'Annabeauty')
+							
 
 
-print(add_new_course(department_courses, student_course, courses))
+			
 
+							
+			
 
-
-
-
-
-
-
-
-
-
-	
